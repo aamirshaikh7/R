45ccb127b572c39a2825174579ea83443b59b436
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-03-27 16:02:53 +0530

    List - creation, exclusion and accessing

---
+++ List/list.r
@@ -0,0 +1,19 @@
+list <- list(1, 2, 3)
+
+typeof(list)
+
+str(list)
+
+# accessing list element
+
+list[c(1)]
+
+list[c(1:3)]
+
+# excluding list element
+
+list[-1]
+
+# accessing list elements using logical vector
+
+list[c(T, T, T)]
